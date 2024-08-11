9181a9fe34123304a3b689dd4e1195ed2a060835
Alfred Augustine <77981488+AsbestosLampshade@users.noreply.github.com>
2024-08-11 13:52:21 +0530

    Update AHT10.h

--- AHT10.h
+++ AHT10.h
@@ -1,15 +1,19 @@
 #ifndef AHT10_H_INCLUDED
-#define AHT_10_H_INCLUDED
+#define AHT_10_H
 #include <Wire.h>
-
 #define byte uint8_t
 #define AHT10_ADDR 0x38
 
-struct HumTem{
+typedef struct {
   float humidity;
   float temperature;
-}HumTem;
+}HumTemp_t;
 
-HumTem captureVal();
+class AHT10{
+  public:
+    void captureVal();
+    HumTemp_t captured;
+};
+//HumTemp_t captureVal();
 
 #endif
