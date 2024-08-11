06738a9c5bce5f365fdddd58f99e2c94a17c6b09
Alfred Augustine <77981488+AsbestosLampshade@users.noreply.github.com>
2024-08-11 13:52:00 +0530

    Update AHT10.cpp

--- AHT10.cpp
+++ AHT10.cpp
@@ -2,38 +2,32 @@
 
 byte b[]={0xAC,0x33,0x00};
 
-HumTem captureVal(){
+void AHT10::captureVal(){
   unsigned long humidity=0;
   unsigned long temperature=0;
-  HumTem out;
-  //Request Data
-  Wire.beginTransmission(0x38);
+  HumTemp_t out;
+  //Request Data from AHT10
+  Wire.beginTransmission(AHT10_ADDR);
   Wire.write(b,3);
   Wire.endTransmission(true);
-  //delay(100);
-  Wire.requestFrom(0x38,7,true);
+  Wire.requestFrom(AHT10_ADDR,7,true);
 
   //Capture Data
-  byte i= Wire.read();// State Value, can ignore
+  byte inData= Wire.read();// State Value, can ignore
 
-  i= Wire.read();
-  humidity=((humidity|i)<<8);
-  i= Wire.read();
-  humidity=((humidity|i)<<4);
-  i= Wire.read();
-  humidity=((humidity|(i>>4)));
-  temperature=((temperature|(i&0x0F))<<8);
-  i= Wire.read();
-  temperature=((temperature|i)<<8);
-  i= Wire.read();
-  temperature=((temperature|i));
+  inData= Wire.read();
+  humidity=((humidity|inData)<<8);
+  inData= Wire.read();
+  humidity=((humidity|inData)<<4);
+  inData= Wire.read();
+  humidity=((humidity|(inData>>4)));
+  temperature=((temperature|(inData&0x0F))<<8);
+  inData= Wire.read();
+  temperature=((temperature|inData)<<8);
+  inData= Wire.read();
+  temperature=((temperature|inData));
 
   //Convert Data
-  out.humidity=humidity;
-  out.humidity=(out.humidity/1048576)*100;
-  out.temperature=temperature;
-  out.temperature=((out.temperature/1048576)*200)-50;
-  // Serial.println(out.humidity);
-  // Serial.println(out.temperature);
-  return out;
+  captured.humidity=((double)humidity/1048576)*100;
+  captured.temperature=(((double)temperature/1048576)*200)-50;
 }
